31e075cb9b51b40268ce402db9e802549661d3d1
dishantcodex5 <dishantbhavsar58@gmail.com>
2025-07-29 11:51:04 +0530

    Add files via upload

---
+++ ARRAY/69. Sqrt(x).py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        
+        square_root=int(sqrt(x))
+        return square_root
+        
